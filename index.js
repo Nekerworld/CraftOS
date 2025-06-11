b4455a3893a2af1e8ecf8e6eaee819c61d4d1c7b
Nekerworld <chrisabc94@gmail.com>
2025-06-12 07:43:45 +0900

    add: 봇 연결 및 에러 처리 로직 추가: 봇 생성 시 연결 에러, 종료, 추방 처리 및 연결 성공 시 환영 메시지 전송 기능 구현

--- index.js
+++ index.js
@@ -5,9 +5,33 @@ const options = {
   username: 'CraftOS',
   version: '1.21.4',
 }
-const bot = mineflayer.createBot(options)
 
-const welcome = () => {
-  bot.chat('안녕하세요!')
+let bot = null
+
+const createBot = () => {
+  bot = mineflayer.createBot(options)
+  
+  // 연결 에러 처리
+  bot.on('error', (err) => {
+    console.error('봇 에러 발생:', err)
+  })
+
+  // 연결 종료 처리
+  bot.on('end', () => {
+    console.log('봇 연결이 종료되었습니다. 재연결을 시도합니다...')
+    setTimeout(createBot, 5000) // 5초 후 재연결 시도
+  })
+
+  // 일반적인 에러 처리
+  bot.on('kicked', (reason) => {
+    console.log('봇이 서버에서 추방되었습니다:', reason)
+  })
+
+  // 연결 성공 시
+  bot.once('spawn', () => {
+    console.log('봇이 성공적으로 연결되었습니다!')
+    bot.chat('안녕하세요!')
+  })
 }
-bot.once('spawn', welcome)
+
+createBot()
